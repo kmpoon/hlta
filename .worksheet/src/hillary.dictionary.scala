6fc1deef07d7a2deab9c2341b7aa2ebc2bf5821f
Leonard Poon <kmpoon@thomas.local>
2016-03-09 16:01:07 +0800

    removed generated source files for worksheet

--- .worksheet/src/hillary.dictionary.scala
+++
@@ -1,6 +0,0 @@
-package hillary
-
-object dictionary {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(80); 
-  println("Welcome to the Scala worksheet")}
-  
-}
