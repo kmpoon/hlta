a32059ab4b71b17c481fb3c928854b2a5417c609
Leonard Poon <kmpoon@eduhk.hk>
2017-02-23 06:21:35 +0800

    fixed the bug that called the wrong class in StepwiseEMHLTA

--- src/main/java/StepwiseEMHLTA.java
+++ src/main/java/StepwiseEMHLTA.java
@@ -2,6 +2,6 @@
 public class StepwiseEMHLTA {
 
     public static void main(String[] args) throws Exception {
-        clustering.StochasticPEM.main(args);
+        clustering.StepwiseEMHLTA.main(args);
     }
 }
